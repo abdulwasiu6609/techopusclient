f42b8b1208357fc8fcf25655b59134b4fe5173e8
Adebayo Abdul wasiu <abdulwasiu6609@gmail.com>
2023-07-02 05:36:29 -0700

    edit

--- src/pages/Privacy Policy.js
+++ src/pages/Privacy Policy.js
@@ -1,73 +1,58 @@
-export default function PrivacyPolicyPage(){
-    return(
-         <div class="post">
-        <h3> Privacy Policy</h3>
-        <div>This Privacy Policy describes how TechOpus ("we," "us," or "our") collects, uses, and discloses 
-personal information when you use our website. Protecting your privacy and personal data is of
-utmost importance to us. By using TechOpus, you consent to the practices outlined in this Privacy Policy.</div>
-        <p>
-            <ol>
-                <li>Information We Collect:</li>
-                <ol type="a"><br>
-                    <li>Personal Information: When you voluntarily provide it to us, we may collect personal information such as
-                your name, email address, and any other information you choose to provide.</li>
-                    <li>Usage Information: We may collect non-personally identifiable information about your interaction
-             with TechOpus, including your IP address, browser type, device information, and browsing activities.</li>
-                </ol><br>
-                <li>Use of Information:</li>
-                <ol type="a"><br>
-                    <li>We use the collected information to:</li>
-                    <ul><br>
-                        <li>Provide and improve our services, content, and user experience on TechOpus.</li>
-                        <li>Respond to your inquiries, comments, or requests.</li>
-                        <li>Communicate with you about updates, promotions, or other relevant information.</li>
-                    </ul>
-                    <li>We do not sell, trade, or rent your personal information to
-         third parties without your explicit consent, except as described in this Privacy Policy.</li>
-                </ol><br>
-                <li>Cookies and Similar Technologies:</li>
-                <ol type="a">
-                    <li>TechOpus may use cookies, web beacons, and similar technologies 
-                        to enhance your browsing experience, personalize content, and gather usage information.</li>
-                    <li>You can manage your cookie preferences through your browser settings. Please note that
-             disabling cookies may impact certain features and functionality of TechOpus.</li>
-                </ol><br>
-                <li>Third-Party Services and Links:</li>
-                <ol type="a"><br>
-                    <li>TechOpus may contain links to third-party websites, plugins, or services that are not operated or controlled by us. We are not responsible for the privacy practices or
-                         content of these third parties. We encourage you to review their respective privacy policies.</li>
-                    <li>TechOpus participates in the Google AdSense program. As a result, Google may collect and use your information for ad personalization.
-                 Please refer to Google's Privacy Policy for more information on how they handle your data.</li>
-                </ol><br>
-                <li>Data Retention:</li>
-                <ol type="a"><br>
-                    <li>We retain personal information for as long as necessary to fulfill the purposes 
-                 in this Privacy Policy, unless a longer retention period is required or permitted by law.</li>
-                    <li>If you wish to request the deletion of your personal information,
-                 please contact us using the information provided below.</li>
-                </ol><br>
-                <li>Data Security:</li>
-                <ol type="a"><bR>
-                    <li>We implement reasonable security measures to protect your 
-                        personal information from unauthorized access, disclosure, alteration, or destruction.</li>
-                    <li>However, please note that no method of transmission over the internet or electronic storage is 
-                100% secure. We cannot guarantee the absolute security of your information. </li>
-                </ol><br>
-                <li>Children's Privacy:</li>
-                <p>TechOpus is not intended for individuals under the age of 16. We do not knowingly collect personal information from children. If you believe
-             we have inadvertently collected information from a child, please contact us immediately.</p>
-                <li>Changes to the Privacy Policy:</li>
-                <p>We reserve the right to update or modify this Privacy Policy at any time. 
-                    Any changes will be effective immediately upon posting the revised Privacy Policy on TechOpus. It is your responsibility to review this Privacy Policy periodically for any updates.</p>
-                <li>Contact Us:</li>
-                <p>If you have any questions, concerns, or requests regarding 
-                    this Privacy Policy or our data practices, please contact us at [email address].</p>
-             </ol>
-                <h3>Last updated: [date]</h3>
-                <p>Please note that this Privacy Policy should be reviewed and adapted to comply with applicable laws and regulations in your jurisdiction. 
-                It is recommended to consult legal counsel to ensure compliance with all relevant privacy laws.</p>
-        </p>
-     </div>
+export default function PrivacyPolicyPage() {
+   return (
+      <div className="pages">
+         <h3> Privacy Policy</h3>
+         <p>This Privacy Policy describes how TechOpus ("we," "us," or "our") collects, uses, and discloses personal information when you use our website. Protecting your privacy and personal data is of utmost importance to us. By using TechOpus, you consent to the practices outlined in this Privacy Policy.</p>
 
-    );
+         <ol>
+            <li>Information We Collect:</li>
+            <ol type="a">
+               <li>Personal Information: When you voluntarily provide it to us, we may collect personal information such as your name, email address, and any other information you choose to provide.</li>
+               <li>Usage Information: We may collect non-personally identifiable information about your interaction with TechOpus, including your IP address, browser type, device information, and browsing activities.</li>
+            </ol>
+
+            <li>Use of Information:</li>
+            <ol type="a">
+               <li>We use the collected information to:</li>
+               <ul>
+                  <li>Provide and improve our services, content, and user experience on TechOpus.</li>
+                  <li>Respond to your inquiries, comments, or requests.</li>
+                  <li>Communicate with you about updates, promotions, or other relevant information.</li>
+               </ul>
+               <li>We do not sell, trade, or rent your personal information to third parties without your explicit consent, except as described in this Privacy Policy.</li>
+            </ol>
+
+            <li>Cookies and Similar Technologies:</li>
+            <ol type="a">
+               <li>TechOpus may use cookies, web beacons, and similar technologies to enhance your browsing experience, personalize content, and gather usage information.</li>
+               <li>You can manage your cookie preferences through your browser settings. Please note that disabling cookies may impact certain features and functionality of TechOpus.</li>
+            </ol>
+            <li>Third-Party Services and Links:</li>
+            <ol type="a">
+               <li>TechOpus may contain links to third-party websites, plugins, or services that are not operated or controlled by us. We are not responsible for the privacy practices or content of these third parties. We encourage you to review their respective privacy policies.</li>
+               <li>TechOpus participates in the Google AdSense program. As a result, Google may collect and use your information for ad personalization. Please refer to Google's Privacy Policy for more information on how they handle your data.</li>
+            </ol>
+            <li>Data Retention:</li>
+            <ol type="a">
+               <li>We retain personal information for as long as necessary to fulfill the purposes in this Privacy Policy, unless a longer retention period is required or permitted by law.</li>
+               <li>If you wish to request the deletion of your personal information, please contact us using the information provided below.</li>
+            </ol>
+            <li>Data Security:</li>
+            <ol type="a">
+               <li>We implement reasonable security measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction.</li>
+               <li>However, please note that no method of transmission over the internet or electronic storage is 100% secure. We cannot guarantee the absolute security of your information. </li>
+            </ol>
+            <li>Children's Privacy:</li>
+            <p>TechOpus is not intended for individuals under the age of 16. We do not knowingly collect personal information from children. If you believe we have inadvertently collected information from a child, please contact us immediately.</p>
+            <li>Changes to the Privacy Policy:</li>
+            <p>We reserve the right to update or modify this Privacy Policy at any time. Any changes will be effective immediately upon posting the revised Privacy Policy on TechOpus. It is your responsibility to review this Privacy Policy periodically for any updates.</p>
+            <li>Contact Us:</li>
+            <p>If you have any questions, concerns, or requests regarding this Privacy Policy or our data practices, please contact us at [email address].</p>
+         </ol>
+         <h3>Last updated: [date]</h3>
+         <p>Please note that this Privacy Policy should be reviewed and adapted to comply with applicable laws and regulations in your jurisdiction. It is recommended to consult legal counsel to ensure compliance with all relevant privacy laws.</p>
+
+      </div>
+
+   );
 }
--- src/pages/Terms and Condition.js
+++ src/pages/Terms and Condition.js
@@ -1,80 +1,58 @@
-export default function TermsAndConditionPage(){
-    return(
-       
-              <div class="pages">
-        <h3>Terms and Conditions</h3>
-       <div> Welcome to TechOpus! 
-        These terms and conditions govern your use of our website and its associated services. <br>
-         By accessing or using TechOpus, you agree to be bound by these terms and conditions.
-          If you do not agree with any part of these terms, please refrain from using our website.</div>
-         <p>
+export default function TermsAndConditionPage() {
+    return (
+
+        <div class="pages">
+            <h3>Terms and Conditions</h3>
+            <p>Welcome to TechOpus!</p>
+            <p>These terms and conditions govern your use of our website and its associated services.By accessing or using TechOpus, you agree to be bound by these terms and conditions. If you do not agree with any part of these terms, please refrain from using our website</p>
+
             <ol>
-                <li>Content and Intellectual Property Rights:</li> <br>
+                <li>Content and Intellectual Property Rights:</li>
                 <ol type="a">
-                    <li>All content on TechOpus, including articles, images, videos, and graphics, is protected by intellectual
-                         property laws and belongs to TechOpus or its content providers.</li>
-                    <li>You may not reproduce, distribute, modify, or display our
-                         content without obtaining prior written permission from us.</li> <br>
+                    <li>All content on TechOpus, including articles, images, videos, and graphics, is protected by intellectual property laws and belongs to TechOpus or its content providers.</li>
+                    <li>You may not reproduce, distribute, modify, or display our content without obtaining prior written permission from us.</li>
                 </ol>
-                <li>User Conduct:</li> <br>
+                <li>User Conduct:</li>
                 <ol type="a">
-                    <li>You agree to use TechOpus for lawful purposes and in a manner 
-                        that does not violate any applicable laws or regulations.</li>
-                    <li>You are responsible for the accuracy and legality
-                         of any content you submit, including comments and contributions.</li>
-                    <li>You must not engage in any activity that disrupts or interferes
-                         with the functioning of TechOpus or its users’ experience.</li> <br>
+                    <li>You agree to use TechOpus for lawful purposes and in a manner that does not violate any applicable laws or regulations.</li>
+                    <li>You are responsible for the accuracy and legality of any content you submit, including comments and contributions.</li>
+                    <li>You must not engage in any activity that disrupts or interferes with the functioning of TechOpus or its users' experience.</li>
                 </ol>
-                <li>Privacy and Data Collection:</li> <br>
+                <li>Privacy and Data Collection:</li>
                 <ol type="a">
-                    <li>We collect and process personal data in accordance with our Privacy Policy. 
-                        By using TechOpus, you consent to such data collection and processing.</li>
-                    <li>We may use cookies and similar technologies to enhance your browsing experience.
-                         You can manage your cookie preferences through your browser settings.</li>
-                </ol> <br>
+                    <li>We collect and process personal data in accordance with our Privacy Policy. By using TechOpus, you consent to such data collection and processing.</li>
+                    <li>We may use cookies and similar technologies to enhance your browsing experience. You can manage your cookie preferences through your browser settings.</li>
+                </ol>
                 <li>Third-Party Websites and Content:</li>
-                <ol type="a"> <br>
-                    <li>TechOpus may contain links to third-party websites or display third-party content. We do not endorse or
-                         take responsibility for the accuracy, content, or practices of these third parties.</li>
-                    <li>Your interactions with third-party websites or content are governed by their respective terms and policies. 
-                        We encourage you to review them before engaging with any third-party services.</li>
-                </ol> <br>
+                <ol type="a">
+                    <li>TechOpus may contain links to third-party websites or display third-party content. We do not endorse or take responsibility for the accuracy, content, or practices of these third parties.</li>
+                    <li>Your interactions with third-party websites or content are governed by their respective terms and policies. We encourage you to review them before engaging with any third-party services.</li>
+                </ol>
                 <li>Disclaimer of Warranties:</li>
-                <ol type="a"> <br>
-                    <li>TechOpus provides information and content on an “as is” and “as available” basis. We make no warranties or representations, express or
-                         implied, regarding the accuracy, reliability, or suitability of the content.</li>
-                    <li>We do not guarantee uninterrupted access to TechOpus or its services, and we are not liable
-                     for any damages or losses resulting from the use or inability to use our website.</li>
-                </ol><br>
+                <ol type="a">
+                    <li>TechOpus provides information and content on an “as is” and “as available” basis. We make no warranties or representations, express or implied, regarding the accuracy, reliability, or suitability of the content.</li>
+                    <li>We do not guarantee uninterrupted access to TechOpus or its services, and we are not liable for any damages or losses resulting from the use or inability to use our website.</li>
+                </ol>
                 <li>Google AdSense:</li>
-                <ol type="a"> <br>
-                    <li>TechOpus participates in the Google AdSense program. As a user, you acknowledge 
-                     and agree that Google may serve personalized ads based on your browsing behavior.</li>
-                    <li>By using TechOpus, you consent to the use of cookies 
-                     and data collection for ad personalization purposes as outlined in our Privacy Policy.</li>
-                </ol> <br>
+                <ol type="a">
+                    <li>TechOpus participates in the Google AdSense program. As a user, you acknowledge and agree that Google may serve personalized ads based on your browsing behavior.</li>
+                    <li>By using TechOpus, you consent to the use of cookies and data collection for ad personalization purposes as outlined in our Privacy Policy.</li>
+                </ol>
                 <li>Modifications and Termination:</li>
-                <ol type="a"> <br>
-                    <li>We reserve the right to modify or discontinue 
-                    any aspect of TechOpus at any time, without prior notice.</li>
-                    <li>We may update these terms and conditions periodically,
-                     and it is your responsibility to review them regularly for any changes.</li>
-                </ol> <br>
-                 <li>Governing Law and Jurisdiction:</li>
-                 <ol type="a"> <br>
-                    <li>These terms and conditions are governed by 
-                    and construed in accordance with the laws of [your jurisdiction].</li>
-                    <li>Any disputes arising from your use of TechOpus shall be subject to
-                     the exclusive jurisdiction of the courts in [your jurisdiction].</li> <br>
-                 </ol>
+                <ol type="a">
+                    <li>We reserve the right to modify or discontinue any aspect of TechOpus at any time, without prior notice.</li>
+                    <li>We may update these terms and conditions periodically, and it is your responsibility to review them regularly for any changes.</li>
+                </ol>
+                <li>Governing Law and Jurisdiction:</li>
+                <ol type="a">
+                    <li>These terms and conditions are governed by and construed in accordance with the laws of [your jurisdiction].</li>
+                    <li>Any disputes arising from your use of TechOpus shall be subject to the exclusive jurisdiction of the courts in [your jurisdiction].</li>
+                </ol>
             </ol>
-         </p><br>
-         <p>
-            By using TechOpus, you agree to abide by these terms and conditions. If you have
-          any questions or concerns, please contact us at [email address].
-            </p> <br>
+
+            <p>By using TechOpus, you agree to abide by these terms and conditions. If you have any questions or concerns, please contact us at [email address].</p>
             <h3>Last updated: [date]</h3>
-    </div>
- 
+        </div>
+
     );
 }
